766b8382da3956341dfd2f6379fafd4e9feefd66
SBS <terdong@naver.com>
2017-12-30 14:57:40 +0900

    첫번째 커밋

---
+++ src/day5/CalculatorTest.java
@@ -0,0 +1,33 @@
+package day5;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 3;
+
+        int result = 0;
+
+        result = add(a,b);
+        System.out.println("a + b = " + result);
+
+        //System.out.println("a - b = " + ?);
+
+        //System.out.println("a * b = " + ? );
+
+        //System.out.println("a / b = " + ? );
+    }
+
+    // 더하기 메소드
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    // 빼기 메소드
+
+    // 곱하기 메소드
+
+    // 나누기 메소드
+
+    // 나머지 메소드
+
+}
