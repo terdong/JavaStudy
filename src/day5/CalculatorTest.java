0f2c1d5ccbcfd049a4066526a0f2de908bfb8f7f
SBS <terdong@naver.com>
2017-12-30 15:16:39 +0900

    첫번째 커밋

--- src/day5/CalculatorTest.java
+++ src/day5/CalculatorTest.java
@@ -10,24 +10,39 @@ public class CalculatorTest {
         result = add(a,b);
         System.out.println("a + b = " + result);
 
-        //System.out.println("a - b = " + ?);
+        result = subtract(a,b);
+        System.out.println("a - b = " + result);
 
-        //System.out.println("a * b = " + ? );
+        result = multiple(a, b);
+        System.out.println("a * b = " + result);
 
-        //System.out.println("a / b = " + ? );
+        result = divide(a,b);
+        System.out.println("a / b = " + result );
+
+        result = remain(a,b);
+        System.out.println("a % b = " + result );
     }
 
     // 더하기 메소드
     public static int add(int a, int b){
         return a + b;
     }
-
     // 빼기 메소드
-
+    public static int subtract(int a, int b){
+        return a - b;
+    }
     // 곱하기 메소드
-
+    public static int multiple(int a, int b){
+        return a * b;
+    }
     // 나누기 메소드
+    public static int divide(int a, int b){
+        return a / b;
+    }
 
     // 나머지 메소드
+    public static int remain(int a, int b){
+        return a % b;
+    }
 
 }
